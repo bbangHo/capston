a8d2f856d84a89085166125583ecd7cb406616a0
bbangHo <sonsins1@naver.com>
2024-04-28 22:37:23 +0900

    feat - 아이템 관련 클래스 정의 #8

---
+++ src/main/java/com/example/capstone/item/repository/ItemRepository.java
@@ -0,0 +1,10 @@
+package com.example.capstone.item.repository;
+
+import com.example.capstone.item.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ItemRepository extends JpaRepository<Item, Long> {
+
+}
