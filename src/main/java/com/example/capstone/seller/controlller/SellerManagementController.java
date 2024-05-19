92f34e1dc5ae1040b8fa552cb112c2c2c70a7413
seongsiyeong <ssy2681q@naver.com>
2024-05-19 20:28:29 +0900

    로그인 및 인증/인가 설정+인가 URL변경(SecurityConfig)+ MEMBERTYPE 수정 + 더미데이터 추가

--- src/main/java/com/example/capstone/seller/controlller/SellerManagementController.java
+++ src/main/java/com/example/capstone/seller/controlller/SellerManagementController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/auth/seller")
+@RequestMapping("/seller")
 public class SellerManagementController {
 
     private final SellerManagementService sellerManagementService;
