14fd9e1e83c427877c9cb32a54a71c3a1d15aa3c
seongsiyeong <ssy2681q@naver.com>
2024-05-25 17:05:11 +0900

    장바구니 담기 기능 추가 및 로그 정보 추가, 인증된 유저만 접근하도록 설정

--- src/main/java/com/example/capstone/order/controller/CartController.java
+++ src/main/java/com/example/capstone/order/controller/CartController.java
@@ -5,12 +5,15 @@ import com.example.capstone.apiPayload.code.status.SuccessStatus;
 import com.example.capstone.order.dto.CartRequestDTO;
 import com.example.capstone.order.service.CartService;
 import com.example.capstone.order.service.CartServiceImpl;
+import com.example.capstone.security.dto.MemberSecurityDTO;
 import com.example.capstone.util.AuthenticatedMemberUtil;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.BindException;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
@@ -28,15 +31,16 @@ public class CartController {
     }
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ApiResponse<String> saveItemInCart(@Valid @RequestBody CartRequestDTO.requestedCart requestedCart, BindingResult bindingResult) throws BindException {
+    public ApiResponse<String> saveItemInCart(@AuthenticationPrincipal MemberSecurityDTO memberSecurityDTO, @Valid @RequestBody CartRequestDTO.requestedCart requestedCart, BindingResult bindingResult) throws BindException {
         log.info("CartController start.........");
 
         if(bindingResult.hasErrors()) {
             log.info("bindException...............");
         }
 
+        //로그인한 유저의 정보를 securityContextHoler에서 불러옵니다.
+        requestedCart.setMemberId(memberSecurityDTO.getId());
 
-        requestedCart.setMemberId(AuthenticatedMemberUtil.getMemberId());
         cartServiceImpl.saveItemInCart(requestedCart);
         log.info("saveItemInCart success.........");
 
